0ae001c8727675098dea9b9fecee016575925386
Guilherme Bueno <gui.guimatheus@hotmail.com>
2024-10-19 21:28:52 -0300

    update

--- VariaveisEparametros.sql
+++ VariaveisEparametros.sql
@@ -43,3 +43,21 @@ SELECT * FROM alugueis WHERE aluguel_id = '10001'
 
 --------
 
+--Trabalhando com Parametros
+
+USE `insight_places`;
+DROP procedure IF EXISTS `insight_places`.`novoAluguel_23`;
+DELIMITER $$
+USE `insight_places`$$
+CREATE DEFINER=`root`@`localhost` PROCEDURE `novoAluguel_23`
+    (vAluguel VARCHAR(10),vCliente VARCHAR(10),vHospedagem VARCHAR(10),vDataInicio DATE,vDataFinal DATE,vPrecoTotal DECIMAL(10,2))
+BEGIN
+    INSERT INTO alugueis VALUES (vAluguel,vCliente,vHospedagem,vDataInicio,vDataFinal,vPrecoTotal);
+END$$
+DELIMITER ;
+
+--Inserindo os dados através dos parametros definidos.
+CALL novoAluguel_23('10002','1003','8635','2023-03-06','2023-03-10','600');
+SELECT * FROM alugueis WHERE aluguel_id = '10003';
+CALL novoAluguel_23('10003','1003','8635','2023-03-10','2023-03-12','250');
+SELECT * FROM alugueis WHERE aluguel_id IN ('10002','10003');
